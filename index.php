1553e106a06f380e73d109a7f298e242d6a0ee45
Nikolaos59 <nikobackland@msn.com>
2023-06-01 13:58:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require('./inc/init.inc.php');
+// Pour le debug
+// var_dump(RACINE_SITE);
+// echo '<br>';
+// echo session_status();
+// Notre accueil
+require('./inc/haut.inc.php');
+echo '<h2>Notre future page d\'accueil pour notre boutique</h2>'; ?>
+
+<h2>Notre page d'accueil</h2>
+<p>Coincée entre le haut et le bas!</p><br>
+<?php
+// // Récupérer la valeur de la variable 
+//  echo getenv('TEST');
+//  echo '<br>ou<br>';
+//  echo $_ENV ['NOM_VARIABLE'];
+
+
+require('./inc/bas.inc.php');
+
